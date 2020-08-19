6a6e1eb0b183c3ba7e83fba035fc3b7f9fb59ca3
Bertking <kingcheeng163@gmail.com>
2020-08-19 17:16:49 +0800

    Create custom_types.rs
    Rust的自定义类型

---
+++ RBE/custom_types.rs
@@ -0,0 +1,9 @@
+/*
+ * @Author: BertKing
+ * @version: 
+ * @Date: 2020-08-19 16:31:25
+ * @LastEditors: BertKing
+ * @LastEditTime: 2020-08-19 16:31:29
+ * @FilePath: /RustConqueror/RBE/custom_types.rs
+ * @Description: 
+ */
