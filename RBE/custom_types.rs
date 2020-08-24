94810d04628f4432d286acab92cbf5cf36c703bf
Bertking <kingcheeng163@gmail.com>
2020-08-24 15:55:52 +0800

    Update custom_types.rs

--- RBE/custom_types.rs
+++ RBE/custom_types.rs
@@ -5,5 +5,10 @@
  * @LastEditors: BertKing
  * @LastEditTime: 2020-08-19 16:31:29
  * @FilePath: /RustConqueror/RBE/custom_types.rs
- * @Description: 
+ * @Description: Rust中的自定义类型
+ * 
+ * > 1. struct ： define a structure 结构体
+ * > 2. enum : define an enumeration 枚举
+ * > 3. 常量可以通过const 和 static 关键字创建。常量 
+ *
  */
