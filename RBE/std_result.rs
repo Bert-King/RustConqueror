4924b782a9a2d84e1e845ad27904fba9cc781974
Bertking <kingcheeng163@gmail.com>
2020-08-26 15:44:20 +0800

    Update std_result.rs
    使用？操作符

--- RBE/std_result.rs
+++ RBE/std_result.rs
@@ -3,11 +3,11 @@
  * @version: 
  * @Date: 2020-08-26 14:23:05
  * @LastEditors: BertKing
- * @LastEditTime: 2020-08-26 14:49:16
+ * @LastEditTime: 2020-08-26 15:44:01
  * @FilePath: /RustConqueror/RBE/std_result.rs
  * @Description: Rust中的枚举 Result
  * 
- * 发展历程:Option<T> ---> Result<T,E>
+ * 发展历程:Option<T> ---> Result<T,E> ---> 操作符？
  * 
  * 我们使用Option<T>作为返回值时，发生错误将会返回None。
  * 但是如果我们需要知道出错的详细信息，这时我们就可以采用枚举Result
@@ -16,8 +16,10 @@
  * 1. Ok(value):表示操作成功。(value:T 返回结果)
  * 2. Err(why): 表示操作失败。(why:E 解释错误)
  * 
+ * 通过下面的例子我们会发现，使用match来得到结果显得复杂混乱,操作符？可以使事情变得更好。
+ * 表达式末尾添加？ === match表达式
+ * 
  */
-
  mod checked{
 
     pub enum MathError{
@@ -73,6 +75,16 @@
         Ok(ratio) => ratio,
     }
 }
+/**
+ * 改善版本
+ */
+fn op_pretty(x:f64,y:f64) -> MathRsult{
+    let radio = div(x,y)?;
+
+    let ln = ln(radio)?;
+}
+
+
 
 
 
