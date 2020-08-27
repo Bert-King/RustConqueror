debd7f342c2b6c4a5f3fc8bf18880d8f3122a45a
Bertking <kingcheeng163@gmail.com>
2020-08-27 16:40:44 +0800

    Create closures.rs
    Rust中的闭包(又称：Lambda表达式)

---
+++ RBE/closures.rs
@@ -0,0 +1,14 @@
+/*
+ * @Author: BertKing
+ * @version: 
+ * @Date: 2020-08-27 16:32:44
+ * @LastEditors: BertKing
+ * @LastEditTime: 2020-08-27 16:34:26
+ * @FilePath: /RustConqueror/RBE/closures.rs
+ * @Description: Rust中的Closures.
+ * 
+ * WTF Closure是什么东西？，其实换个名字我们就很容易理解啦。
+ * 
+ * Closure 等价于 Rust中的Lambda 表达式。
+ * 
+ */
