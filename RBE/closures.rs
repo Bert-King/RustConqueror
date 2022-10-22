46f0f59e78b26d1a4fbbd53d9a021d414b8eae4a
Bertking <kingcheeng163@gmail.com>
2022-10-22 21:49:17 +0800

    some fix

--- RBE/closures.rs
+++ RBE/closures.rs
@@ -3,12 +3,16 @@
  * @version: 
  * @Date: 2020-08-27 16:32:44
  * @LastEditors: BertKing
- * @LastEditTime: 2020-08-27 16:34:26
+ * @LastEditTime: 2020-08-27 16:50:39
  * @FilePath: /RustConqueror/RBE/closures.rs
- * @Description: Rust中的Closures.
+ * @Description: Rust中的Closures(闭包).
  * 
  * WTF Closure是什么东西？，其实换个名字我们就很容易理解啦。
  * 
- * Closure 等价于 Rust中的Lambda 表达式。
+ * Closure(闭包) 等价于 Rust中的Lambda 表达式。
+ * Closures in Rust, also called lambda expressions or lambdas,
+ * 
+ * 
+ * 
  * 
  */
